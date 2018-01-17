d41bf98751292088fde4b31225efcaf8faf3d141
Kurt Höblinger <office@nitricware.com>
2018-01-17 17:07:10 +0100

    fixed: class name and filename did not match

--- Assets/NWScripts/DisplayHint.cs
+++ Assets/NWScripts/DisplayHint.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WasGuckstDu : MonoBehaviour
+public class DisplayHint : MonoBehaviour
 {
     public Text hintBox;
     private Dictionary<string, string> labels;
